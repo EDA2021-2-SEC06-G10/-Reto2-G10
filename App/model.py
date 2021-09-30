4fc76b33b029c478bdc45fd46966dfbb91dd15e3
JOSE NICOLAS CARDENAS TRUJILLO <j.cardenast@uniandes.edu.co>
2021-09-29 23:45:15 -0500

    Primera entrega – Reto 2

--- App/model.py
+++ App/model.py
@@ -24,6 +24,9 @@
  * Dario Correal - Version inicial
  """
 
+#####-----#####-----#####-----#####-----#####   ####---#####---####   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   IMPORTACIÓN MÓDULOS   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   ####---#####---####   #####-----#####-----#####-----#####-----#####
 
 import config as cf
 from DISClib.ADT import list as lt
@@ -32,19 +35,248 @@ from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Sorting import shellsort as sa
 assert cf
 
+
+
+
+#####-----#####-----#####-----#####-----#####   ########-----######-----########   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   DEFINICIÓN ESTRUCTURAS ELEMENTOS   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   ########-----######-----########   #####-----#####-----#####-----#####-----#####
+
 """
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
+    Se define la estructura que contiene el catálogo del museo.
+    Este tendrá dos listas, una para los artistas y otra para las obras.
+
 """
 
-# Construccion de modelos
+# Función que crea el catálogo.
+def new_Catalog () -> dict:
+    """
+        Esta función permite crear la estructura que guarda el catálogo del museo.
+        
+        Crea dos listas vacías que permitirán guardar los siguientes elementos:
+         1- Los artistas.
+         2- Las obras.
+
+        Además, esta función crea un índice por técnica ("Mediums") de cada obra.
+
+
+        No tiene parámetros.
+
+        Retorno:
+            -> (dict): el catálogo del museo.
+
+    """
+
+    # Definir variable que guarda la información del catálogo e inicializarla.
+    catalog = {'artworks': None,
+                'Mediums': None}
+
+
+    #####-----#####-----#####-----#####   Definición Listas Elementos   #####-----#####-----#####-----#####
+
+    """
+        La siguiente lista contiene todas los obras encontradas en 
+        los archivos de carga. Estas no estás ordenadas bajo ningún 
+        criterio, y son referenciadas por los índices que se definirán
+        en seguida.
+    
+    """
+    catalog['artworks'] = lt.newList('SINGLE_LINKED')            # Pendiente añadir función de comparación.
+
+
+    #####-----#####-----#####   Definición Índices   #####-----#####-----#####
+
+    """
+        A continuacion se crearán índices por diferentes criterios
+        para llegar a la informacion requerida. Estos índices no
+        replican informacion, solo referencian los libros de la lista
+        creada anteriormente.
+    
+    """
+    
+    # Índice que crea un map cuya llave es la técnica las obras.
+    catalog['Mediums'] = mp.newMap(4000,
+                                  maptype='PROBING',
+                                  loadfactor=0.5,
+                                  comparefunction="") # Pendiente añadir función de comparación del map.
+
+
+    #####-----#####-----#####-----#####   Retorno   #####-----#####-----#####-----#####
+
+    return catalog
+
 
 # Funciones para agregar informacion al catalogo
 
-# Funciones para creacion de datos
 
-# Funciones de consulta
 
-# Funciones utilizadas para comparar elementos dentro de una lista
 
-# Funciones de ordenamiento
+
+#####-----#####-----#####-----#####-----#####   ###---###----###   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   ADICIÓN DE DATOS   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   ###---###----###   #####-----#####-----######-----####-----#####
+
+"""
+    Se define las funciones que permitirán añadir elementos al catálogo.
+
+"""
+
+# Función que agrega una obra al catálogo.
+def add_artwork (catalog: dict, artwork: dict) -> None:
+    """
+        Esta función permite agregar una obra al catálogo, guardándolo en el arreglo 'obras'.
+
+        Parámetros:
+            -> catalog (dict): catálogo.
+            -> artwork (dict): obra que se quiere adicionar. 
+
+        No tiene retorno.
+
+    """
+    
+    # Crear una obra con los datos ingresados por parámetro.
+    artwork_new = new_artwork(artwork)
+
+    # Agregar la obra a la última posición de la lista "obras".
+    lt.addLast(catalog['artworks'], artwork_new)
+
+
+
+# Función que agrega una obra al índice de técnicas.
+def add_medium (catalog: dict, artwork: dict) -> None:
+    """
+        Esta función permite agregar al índice "Mediums" del catálogo una pareja llave-valor 
+        referente a una técnica y obra determinadas.
+
+        Parámetros:
+            -> catalog (dict): catálogo.
+            -> artwork (dict): obra.
+
+        No tiene retorno.
+
+    """
+
+    # Crear variable que guarda el map 'Mediums' del catálogo.
+    mediums = catalog['Mediums']
+
+    # Guardar técnica de la obra ingresada por parámetro.
+    medium_artwork = artwork['Medium']
+   
+    # Si la obra tiene una técnica registrada.
+    if (medium_artwork != ''):
+        # Añadir obra.
+        mp.put(mediums, medium_artwork, artwork) 
+
+
+
+
+
+#####-----#####-----#####-----#####-----#####   ###---####----###   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   CREACIÓN DE DATOS   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   ###---####----###   #####-----#####-----######-----####-----#####
+
+"""
+    Se define las funciones que permitirán crear elementos referentes a información
+    de interés del catálogo, como los artistas, las obras, las técnicas, entre otros.
+
+"""
+
+# Función que crea una nueva obra.
+def new_artwork (artwork_info: dict) -> dict:
+    """
+        Esta función permite crear una obra. Estas se representarán mediante 
+        el tipo de dato dict de Python.
+
+        Parámetros:
+            -> info_artwork (dict): diccionario que tiene toda la información de la obra que
+                                 se encuentra en la base de datos.
+
+        Retorno:
+            -> (dict): diccionario que representa a la obra.
+
+    """
+
+    # Crear variable que guarda el diccionario con la información de interés de la obra.
+    artwork = {"ObjectID": "",
+               "Title": "",
+               "ConstituentID": None,
+               "Date": "",
+               "Medium": "",
+               "Classification": "",
+               "DateAcquired": "",
+               "Circumference (cm)": "",
+               "Depth (cm)": "",
+               "Diameter (cm)": "",
+               "Height (cm)": "",
+               "Length (cm)": "",
+               "Weight (kg)": "",
+               "Width (cm)": "",
+               "Seat Height (cm)": ""}
+
+
+    # Crear variable que guarda la lista de los id de los atistas que crearon la obra y asignarle la lista
+    # que contiene dichos datos.
+    list_artists_id = []
+    list_artists_id = turn_into_list(artwork_info["ConstituentID"])
+
+
+    # Iteración que añade la información de la obra.
+    for property in artwork.keys():
+
+        # Determinar si la propiedad actual es "ConstituentID".
+        is_ConstituentID = (property == "ConstituentID")
+
+        # Si la obra no tiene la propiedad actual.
+        if (artwork[property] == ""):
+            # Asignar el valor de la propiedad como "".
+            artwork[property] = ""
+
+        # De lo contrario
+        else:            
+            # Añadir propiedad.
+            if not(is_ConstituentID):
+                artwork[property] = artwork_info[property]
+            else:
+                artwork[property] = list_artists_id
+
+
+    # Retornar obra.
+    return artwork
+
+
+
+
+
+#####-----#####-----#####-----#####-----#####   #####---####----#####   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   FUNCIONES ADICIONALES   #####-----#####-----#####-----#####-----#####
+#####-----#####-----#####-----#####-----#####   #####---####----#####   #####-----#####-----#####-----#####-----#####
+
+"""
+    A continuación se definen funciones que serán necesarias para desarrollar los
+    requerimientos, o que serán de utilidad en general.
+
+"""
+
+# Función que convierte el str de la lista de artistas en una lista.
+def turn_into_list (list_in_str: str) -> list:
+    """
+        Esta función permite convertir la cadena de texto que guarda los id de los artistas que crearon
+        una obra en una lista.
+
+        Parámetros:
+            -> list_in_str (str): cadena de texto con la lista de los id de los artistas.
+
+        Retorno:
+            -> (list): lista con los id de los artistas que crearon la obra.
+
+    """
+
+    # Crear lsta vacía en la que se guardarán los id de los artistas.
+    artists_list = []
+
+    # Crear sublista que contenga solo los id de los artistas mediante slicing y la función split().
+    # Se hace parar borra los caracteres "[" y "]" que se encuentran al final de la cadena.
+    artists_list = (list_in_str[1 : len(list_in_str) - 1]).split(",")
+
+    # Retornar la lista.
+    return artists_list
--- README.md
+++ README.md
@@ -1,18 +1,44 @@
-#ISIS1225 - Librerias de soporte
+Integrantes:
+ <> José Nicolás Cárdenas - 201922006 - j.cardenast@uniandes.edu.co
+ <> Andrés Leonardo Beltran - 202014143 - al.beltran@uniandes.edu.co
 
-Este proyecto contiene los Tipos abstractos de datos, estructuras de datos y algoritmos requeridos por el curso ISIS1225-Estructuras de Datos y Algoritmos
+Requerimientos:
+ Req. 3 - Andrés Leonardo Beltrán
+ Req. 4 - José Nicolás Cárdenas
 
--Lib
-Este directorio contiene todo el código de base que se entrega para el funcionamiento del curso.  Dentro de este directorio encuentran:
-    
-    |-- ADT:  Directorio con los Tipos Abstractos de Datos del curso
 
-    |--DataStructures: Directorio con todas las estructuras de datos y archivos auxiliares para su     correcto funcionamiento
 
-    |--Algorithms: Implementación de los algoritmos vistos en el curso (por ejemplo algoritmos de ordenamiento)
+#####-----#####-----#####   Modificaciones   #####-----#####-----#####
 
-    |-- Utils: Funciones auxiliares para el funcionamiento de los TADs y estructuras de datos
 
--Test
-Este directorio contiene las pruebas realizadas a los TADs, Estructuras de Datos y Algoritmos.
+##--##--##  General  ##--##--##
 
+ 1- Se añadió la carpeta de datos (MoMA) al repositorio.
+
+
+##--##--##  Model  ##--##--##
+
+ 1- Se actualizó la documentación relacionada a la estructura del catálogo.
+
+ 2- Se creó la función newCatalog():
+    > Se creó la lista enlazada que guardará la información de las obras.
+    > Se creó el índice que permitirá identificar los medios o técncias de lass obras.
+
+ 3- Se definió la función de creación función new_artwork().
+
+ 4- Se definieron las funciones de adición add_artwork() y add_medium().
+
+
+
+#####-----#####-----#####   Pendiente   #####-----#####-----#####
+
+
+##--##--##  Model  ##--##--##
+
+ 1- De la función newCatalog():
+    > Actualizar el cuerpo para poder guardar la información de los artistas.
+    > Añadir las funciones de comparación de la lista 'artworks' y el map 'Medium'.
+
+ 2- De la función add_medium(), añadir bloque de código para añadir obra si la técnica ya existe.
+
+ 3- Implementar el código que permita realizar el siguiente requerimiento: “las n obras más antiguas para un medio específico”.
